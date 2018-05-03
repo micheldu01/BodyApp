f6d7516fd3a2a67bea6b9d70eb7ab80be2ed30f7
michel <laurentmichelst@gmail.com>
2018-05-03 21:43:11 +0200

    CREATE TRAINING ACTIVITY

---
+++ app/src/main/java/com/example/michel/bodyapp/TrainingActivity.java
@@ -0,0 +1,51 @@
+package com.example.michel.bodyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+public class TrainingActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training);
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
