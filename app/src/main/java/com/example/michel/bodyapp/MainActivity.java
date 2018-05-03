670360c3abd732e41e8db3d877f8f5e4dd3208fd
michel <laurentmichelst@gmail.com>
2018-05-03 19:33:03 +0200

    ADD TOOLBAR IN MAIN ACTIVITY

--- app/src/main/java/com/example/michel/bodyapp/MainActivity.java
+++ app/src/main/java/com/example/michel/bodyapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.michel.bodyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
+    // DECLARE TOOLBAR
+    @BindView(R.id.activity_main_toolbar)
+    android.support.v7.widget.Toolbar toolbar;
 
 
     @Override
@@ -16,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        // DECLARE BUTTER KNIFE
+        ButterKnife.bind(this);
+
+        // ADD TOOLBAR
+        this.configureToobar();
 
 
 
@@ -23,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    // METHOD FOR GET TOOLBAR
+    private void configureToobar() {
+
+        // IMPLEMENT TOOLBAR
+        setSupportActionBar(toolbar);
+
+    }
+
+
+
 }
 
 
