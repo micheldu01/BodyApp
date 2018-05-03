7bdddde1fe372855e6b43bb7072e331b1b5e5fd1
michel <laurentmichelst@gmail.com>
2018-05-03 19:23:38 +0200

    ADD GRADLE LIBRARY AND CONNECT FIRE BASE

--- app/src/main/java/com/example/michel/bodyapp/MainActivity.java
+++ app/src/main/java/com/example/michel/bodyapp/MainActivity.java
@@ -3,11 +3,51 @@ package com.example.michel.bodyapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
