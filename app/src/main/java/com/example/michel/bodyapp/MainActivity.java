2d1add871b704341c6fc7c4bdf63787de94123ea
michel <laurentmichelst@gmail.com>
2018-05-03 20:57:18 +0200

    ADD DRAWER MENU IN MAIN ACTIVITY

--- app/src/main/java/com/example/michel/bodyapp/MainActivity.java
+++ app/src/main/java/com/example/michel/bodyapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.michel.bodyapp;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,9 +18,15 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
 
+
     // DECLARE TOOLBAR
     @BindView(R.id.activity_main_toolbar)
     android.support.v7.widget.Toolbar toolbar;
+
+    // DECLARE DRAWER LAYOUT
+    @BindView(R.id.activity_main_drawer_layout)DrawerLayout drawerLayout;
+
+
 
 
     @Override
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         // ADD TOOLBAR
         this.configureToobar();
 
+        // ADD DRAWER LAYOUT
+        this.configureDrawerLayout();
 
 
 
@@ -46,20 +56,43 @@ public class MainActivity extends AppCompatActivity {
 
         // IMPLEMENT TOOLBAR
         setSupportActionBar(toolbar);
-
     }
 
 
-    // IMPLEMENT BUTTON SEARCH IN TOOLBAR
+    // IMPLEMENT MENU IN TOOLBAR
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        // IMPLEMENT THE BUTTON INTO THE TOOLBAR
+        // IMPLEMENT THE MENU LAYOUT INTO THE TOOLBAR
         getMenuInflater().inflate(R.menu.menu_activty_main, menu);
         return true;
     }
 
 
+    // IMPLEMENT BUTTON SEARCH INTO MENU TOOLBAR
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // IMPLEMENT SWTICH INTO MENU TOOLBAR
+        switch (item.getItemId()){
+
+            // IMPLEMENT BUTTON SEARCH
+            case R.id.menu_activity_main_search:
+
+                // WHEN SELECT
+                Toast.makeText(this,"Il me reste à implementer le bouton search", Toast.LENGTH_SHORT).show();
+        }
+        // FINISH
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // IMPLEMENT DRAWER LAYOUT
+    private void configureDrawerLayout(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
 }
 
